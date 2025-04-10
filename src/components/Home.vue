ee53a5a1797bc754d1597da41dbe849af6b68f55
Zhengru <mail@bzr.ink>
2025-04-10 17:47:31 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,11 @@
 <template>
   <div class="home">
-    <div class="about">
-      <a href="https://luxirty.com/posts/luxirty-search/" target="_blank">关于</a>
-    </div>
     <div class="logo">
       Luxirty Search
     </div>
     <div class="search-container">
       <div class="gcse-searchbox-only" data-resultsUrl="search"></div>
     </div>
-    
-    <!-- 添加页脚 -->
-    <footer>
-      <p>
-        &copy; Create by <a href="https://luxirty.com/about/" target="_blank">Luxirty</a> with Love |
-        Open Source on <a href="https://github.com/KoriIku/luxiry-search" target="_blank">
-          GitHub
-        </a>
-      </p>
-    </footer>
   </div>
 </template>
 
@@ -68,68 +55,5 @@ export default {
   }
 }
 
-.about {
-  position: absolute;
-  top: 20px;
-  right: 20px;
-}
 
-.about a {
-  text-decoration: none;
-  font-size: 16px;
-  color: var(--uv-styles-color-text-default);
-}
-
-/* 新增的页脚样式 */
-footer {
-  position: absolute;
-  bottom: 20px; /* 离底部20px */
-  text-align: center; /* 居中对齐 */
-  width: 100%; /* 宽度为100% */
-  font-size: 14px; /* 字体大小 */
-  color: var(--uv-styles-color-text-default); /* 使用与其他文本一致的颜色 */
-}
-
-/* 链接样式 */
-footer a {
-  color: #156bc8; /* 白天模式下的鲜明蓝色 */
-  font-weight: bold; /* 加粗字体 */
-  transition: color 0.3s ease, text-shadow 0.3s ease; /* 添加文本阴影过渡 */
-}
-
-/* 深色模式适配 */
-@media (prefers-color-scheme: dark) {
-  footer {
-    background-color: #1a1a1a; /* 深色背景 */
-  }
-  
-  footer a {
-    color: #ffffff; /* 明亮的链接颜色 */
-  }
-}
-
-/* 鼠标悬停效果 */
-footer a:hover {
-  color: #0056b3; /* 鼠标悬停时的深蓝色 */
-  text-decoration: underline; /* 添加下划线 */
-  text-shadow: 0 0 5px rgba(0, 123, 255, 0.5); /* 鼠标悬停时添加阴影 */
-}
-
-/* 鼠标悬停效果 - 深色模式 */
-@media (prefers-color-scheme: dark) {
-  footer a {
-    color: #80a0c2;
-    /* 链接颜色改为浅色 */
-  }
-
-  footer a:hover {
-    color: #a0c3e0;
-    /* 悬停时的链接颜色略微加亮 */
-  }
-
-  footer img {
-    opacity: 0.9;
-    /* 提高图像的亮度，适应深色背景 */
-  }
-}
 </style>
